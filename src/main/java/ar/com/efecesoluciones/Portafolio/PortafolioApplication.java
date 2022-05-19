5e327a1b28fdd0c7a80fe4d770dcf5e63499128f
Lojito <38134810+MarianC312@users.noreply.github.com>
2022-05-19 14:25:04 -0300

    test6

--- src/main/java/ar/com/efecesoluciones/Portafolio/PortafolioApplication.java
+++ src/main/java/ar/com/efecesoluciones/Portafolio/PortafolioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = {"localhost", "http://localhost", "http://localhost:4200/"})
+@CrossOrigin(origins = {"https://marianocampos.netlify.app", "localhost", "http://localhost", "http://localhost:4200/"})
 public class PortafolioApplication {
 
 	public static void main(String[] args) {
