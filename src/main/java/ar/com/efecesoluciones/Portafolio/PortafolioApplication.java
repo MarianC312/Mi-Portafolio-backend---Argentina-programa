a97ecf23782081abdea2e152a1ab21f4e631dca3
Lojito <38134810+MarianC312@users.noreply.github.com>
2022-05-19 14:15:40 -0300

    test5

--- src/main/java/ar/com/efecesoluciones/Portafolio/PortafolioApplication.java
+++ src/main/java/ar/com/efecesoluciones/Portafolio/PortafolioApplication.java
@@ -2,8 +2,10 @@ package ar.com.efecesoluciones.Portafolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = {"localhost", "http://localhost", "http://localhost:4200/"})
 public class PortafolioApplication {
 
 	public static void main(String[] args) {
