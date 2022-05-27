ce5b75e492d96703040d18cc8cfe9fa9f86953a5
Lojito <38134810+MarianC312@users.noreply.github.com>
2022-05-27 09:36:50 -0300

    Update JwtEntryPoint
    Corrección mensaje "no autorizado."

--- src/main/java/ar/com/efecesoluciones/Portafolio/seguridad/jwt/JwtEntryPoint.java
+++ src/main/java/ar/com/efecesoluciones/Portafolio/seguridad/jwt/JwtEntryPoint.java
@@ -19,6 +19,6 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         logger.error("Fallo método commence");
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No autorizado.");
     }
 }
